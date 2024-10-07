279f1d1e0304e598b54cdfcfa82056e915d67da2
GuyElm <160611493+GuyElm@users.noreply.github.com>
2024-10-07 10:16:28 +0300

    Add files via upload

---
+++ Lesson01/Example01.java
@@ -0,0 +1,15 @@
+package Lesson01;
+
+import org.testng.annotations.Test;
+
+public class Example01 {
+    @Test
+    public void test01(){
+        System.out.println("Test01");
+    }
+
+    @Test
+    public void test02(){
+        System.out.println("Test02");
+    }
+}
